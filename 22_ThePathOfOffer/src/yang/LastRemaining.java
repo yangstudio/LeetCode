9a50c1a9db890e2735959c476d42b8aee6a8b462
yangchao <yangchao@cqu.com>
2021-06-05 17:07:41 +0800

    V-1.0.1

--- 22_ThePathOfOffer/src/yang/LastRemaining.java
+++ 22_ThePathOfOffer/src/yang/LastRemaining.java
@@ -1,5 +1,7 @@
 package yang;
 
+import java.util.ArrayList;
+
 /**
  * @Description: 剑指 Offer 62. 圆圈中最后剩下的数字
  * https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/
@@ -8,6 +10,17 @@ package yang;
  */
 public class LastRemaining {
     public int lastRemaining(int n, int m) {
-
+        ArrayList<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        int idx = 0;
+        while (n > 1) {
+            idx = (idx + m - 1) % n;
+            list.remove(idx);
+            n--;
+        }
+        return list.get(0);
     }
+
 }
